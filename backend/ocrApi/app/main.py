503269bec4ceb7d070809d6825af5674557e039f
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-11-08 17:10:07 -0300

    parsing image and pdf files on different url paths

--- backend/ocrApi/app/main.py
+++ backend/ocrApi/app/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from ocr import parse_pdf, parse_image, get_image_from_bytes
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
-    message = f"Hello!"
+    message = f"Hello World!"
     return {"message": message}
+
+
+@app.post("/upload-file/")
+async def upload_image(file: bytes = File(...)):
+    image = get_image_from_bytes(file)
+    parsed = parse_image(image)
+    return {"parsed": parsed}
+
+
+@app.post("/upload-pdf/")
+async def upload_pdf(file: bytes = File(...)):
+    parsed = parse_pdf(file)
+    return {"parsed": parsed}
--- backend/ocrApi/app/ocr.py
+++ backend/ocrApi/app/ocr.py
@@ -1,39 +1,65 @@
 """
 Retrieve and process image with the tesseract API
 """
-import pytesseract
 import requests
+import pytesseract
 from PIL import Image
 from PIL import ImageFilter
 from io import BytesIO
 import cv2
 import numpy as np
+import tempfile
+from pdf2image import convert_from_bytes
 
 
-def parse_image(filename):
-    """ convert image with tesseract """
-    image = get_image_from_file(filename)
+def _convert_pdf_to_image(filename):
+    """ Returns a list of Image objects """
+    print("Converting PDF to image")
+    with tempfile.TemporaryDirectory() as path:
+        images = convert_from_bytes(filename, output_folder=path)
+    return images
+
+
+def parse_pdf(filename):
+    """ Parses pdf file with tesseract """
+    out = {}
+    images = _convert_pdf_to_image(filename)
+    for idx, image in enumerate(images):
+        print(f"Parsing page {idx}")
+        out[f"page {idx}"] = parse_image(image)
+    return out
+
+
+def parse_image(image):
+    """ convert PIL Image with tesseract """
     image = process_image(image)
     return pytesseract.image_to_string(image)
 
 
 def process_image(image):
     """ Perform image processing """
-    print('Processing image...')
-    ret,img = cv2.threshold(np.array(image), 125, 255, cv2.THRESH_BINARY)
+    print("Processing image...")
+    _, img = cv2.threshold(np.array(image), 125, 255, cv2.THRESH_BINARY)
     return img
+
 
 def get_image_from_url(url):
     """ open image as PIL object """
-    img = Image.open(BytesIO(requests.get(url).content)).convert('L')
+    img = Image.open(BytesIO(requests.get(url).content)).convert("L")
     return img
+
+
+def get_image_from_bytes(filename):
+    """ open image from file object as PIL object """
+    return Image.open(BytesIO(filename)).convert("L")
+
 
 def get_image_from_file(filename):
     """ open image as PIL object from file system """
-    return Image.open(filename).convert('L')
+    return Image.open(filename).convert("L")
 
 
-if __name__ == '__main__':
-    print('parsing')
-    parsed = parse_image('sample.png')
+if __name__ == "__main__":
+    print("parsing")
+    parsed = parse_image("sample.png")
     print(parsed)
